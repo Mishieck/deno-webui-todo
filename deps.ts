091d1d29b881ca8fc3a1f1ed50f55d873a456126
Mishieck <mishieckmwale@gmail.com>
2023-09-10 20:02:30 +0200

    chore(deps): add dependencies - Add preact-render-to-string for converting JSX to strings. - Add deno-webui for creating the app. - Add emit (deno_emit) for bundling.

---
+++ deps.ts
@@ -0,0 +1,5 @@
+import render from 'https://esm.sh/preact-render-to-string@5.2.6';
+export * from './deno-webui/mod.ts';
+export * from 'https://deno.land/x/emit@0.27.0/mod.ts';
+
+export { render };
