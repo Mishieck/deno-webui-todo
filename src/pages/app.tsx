7528ca827638d58ebabbc65c0501c18d3c0ca47d
Mishieck <mishieckmwale@gmail.com>
2023-09-11 09:58:53 +0200

    feat(pages/app): add app page

---
+++ src/pages/app.tsx
@@ -0,0 +1,20 @@
+/* @jsxImportSource https://esm.sh/preact@10.15.1 */
+
+export const App = () => {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+        />
+        <title>Todo</title>
+      </head>
+      <body>
+        <div id="root">Body</div>
+      </body>
+    </html>
+  );
+};
