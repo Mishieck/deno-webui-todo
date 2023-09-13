a65286d1350f364dfc290833606dfc32decfe441
Mishieck <mishieckmwale@gmail.com>
2023-09-13 11:35:14 +0200

    feat(utils/constants): add constants to utils Add error codes.

---
+++ src/utils/constants.ts
@@ -0,0 +1,8 @@
+export const errorCodes = {
+  DOM_ELEMENT_NOT_FOUND: 'don/element/not-found',
+  DATABASE_WRITE_FAILED: 'database/write/failed',
+  DATABASE_READ_FAILED: 'database/read/failed',
+  DATABASE_UPDATE_FAILED: 'database/update/failed',
+  DATABASE_DELETE_FAILED: 'database/delete/failed',
+  DATABASE_ENTRY_NOT_FOUND: 'database/entry/not-found'
+};
